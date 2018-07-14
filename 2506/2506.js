9dfd6fe2c15cfdb76cad3d496e965adb33dd0dbc
tamunatais <tamari.gogoladze.1@iliauni.edu.ge>
2018-07-14 13:14:56 +0400

    project

---
+++ 2506/2506.js
@@ -0,0 +1,114 @@
+// // // let message;
+// // // message = "hello";
+// // // message
+// // // let user = 'John', age = 25, message = 'Herllo';
+
+
+
+//EXERCISE 1
+
+// // let name = "Sylvia";
+// // let admin = name;
+// // alert(admin);
+
+
+// let name = "Tamuna";
+// alert(`Hello ${name}!`);
+
+
+// embed an expression
+// alert(`the result is ${1 + 3}`);
+
+
+// EXERCISE 2
+
+//let name = "Dan";
+//alert(`Hello ${name + 1}`);
+//alert(`Hello ${"name"}`);
+
+
+
+// let isGreater = 4 > 1;
+// alert(isGreater);
+
+// let x;
+// alert(x);
+
+// let value = true;
+// alert(typeof value);
+
+// alert('3' + 2);
+
+// let str = '123';
+// alert(typeof str);
+// let num = Number(str);
+// alert(typeof num);
+
+// let age = prompt("Enter your age")
+// alert(`your age is ${age}`);
+
+// let isBoss = confirm("Are you the boss?");
+// alert(isBoss);
+
+//EXERCISE 4
+
+// let a = prompt("enter number");
+// let b = prompt("enter another number");
+// alert(Number(a)+Number(b));
+
+
+//EXERCISE 5
+// let a = 1;
+// let b = 1;
+// let c = ++a;
+// let d = c++;
+
+// alert(a); // 2
+// alert(c); // 3
+// alert(d++); //2
+
+
+// let counter = 1;
+// let a = counter++;
+
+// alert(counter);
+
+
+// EXERCISE 6
+// alert(5 > 4); //true
+// alert("apple" > "pineapple");  // false
+// alert("2" > "12");
+// alert(undefined==null); //true
+// alert(undefined===null); //false
+// alert( null == "0"); //false
+// alert( null === "0"); //false
+
+
+let accessAllowed = prompt("Please login");
+    if (accessAllowed = "admin") {
+        prompt("Enter password");
+    }
+
+    else if (access = "" || Esc) {
+    alert("canceled");
+}
+
+else {
+    alert("I don't know you");
+}
+
+let Password = prompt("Enter password");
+
+if (Password = "TheMaster") {
+    alert("Welcome!");
+}
+
+else if (prompt = "" || Canceled) {
+    alert("Cancelled");
+}
+
+else {
+    alert("Wrong Password");
+}
+
+
